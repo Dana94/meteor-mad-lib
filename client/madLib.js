97f1afc8511e6ab7fce3ea8361b9012a299bc37c
Akeem McNaught <akizz129@gmail.com>
2016-08-01 20:20:18 -0400

    added completed madlib

--- client/madLib.js
+++ client/madLib.js
@@ -5,5 +5,16 @@ Template.body.events({
 		event.preventDefault();
 
 		console.log("Hello, world");
+
+		let exclamation = template.find("#exclamation").value;
+
+		let adverb = template.find("#adverb").value;
+
+		let verb = template.find("#verb").value;
+
+		let noun = template.find("#noun").value;
+
+		console.log(exclamation + "! you say, as you " + adverb + " jump onto your " + noun +" and " + verb
+			+ " into the sunset.");
 	} 
 });
